8c0947d63d5f11fa6b21cbce465d32f41116ad36
evillt <hujiehao@deepexi.com>
2020-07-16 12:01:27 +0800

    refactor(nuxt): 使用原版

--- nuxt.js
+++ nuxt.js
@@ -1,14 +1,7 @@
 const UpdatePopup = require('.')
 
-class NuxtUpdatePopup extends UpdatePopup {
-  constructor() {
-    super()
-    this.prefix = '/_nuxt'
-  }
-}
-
 export default function nuxtUpdatePopup() {
   this.extendBuild(config => {
-    config.plugins.push(new NuxtUpdatePopup())
+    config.plugins.push(new UpdatePopup())
   })
 }
