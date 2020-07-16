1edafecd6ed6c728f9d5a54639b65dbab0273afe
evillt <hujiehao@deepexi.com>
2020-07-16 14:43:31 +0800

    chore: types declaration

--- nuxt.js
+++ nuxt.js
@@ -1,5 +1,7 @@
 const UpdatePopup = require('.')
 
+/** @typedef {any} NuxtModule */
+/** @type {(this: NuxtModule) => void} */
 export default function nuxtUpdatePopup() {
   this.extendBuild(config => {
     config.plugins.push(new UpdatePopup())
