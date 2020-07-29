839af7d5e56fcd7875c453bfb0091599bc528289
EVILLT <53422750+eviIIt@users.noreply.github.com>
2020-07-29 10:13:05 +0800

    refactor: 对比版本函数放回去 (#6)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import '@evillt/toast/dist/toast.css'
 import {createToast} from '@evillt/toast'
 
-import {compareVersion} from '{{srcUtils}}'
-
 main()
 
 function main() {
@@ -69,6 +67,20 @@ function main() {
         }
       })
   }
+}
+
+/** @type {(newVersion: string, currentVersion: string) => boolean}*/
+export function compareVersion(newVersion, currentVersion) {
+  if (newVersion && currentVersion) {
+    const n = newVersion.split('.')
+    const c = currentVersion.split('.')
+
+    for (let i = 0; i <= n.length; i++) {
+      if (Number(n[i]) > Number(c[i])) return true
+    }
+  }
+
+  return false
 }
 
 function createInterval(callback) {
